e9d932739c7b953dd1fafc2eff430b9d7ca27cf8
Christina-Tovar-Hamernik <ctovarhamernik@gmail.com>
2019-02-20 01:01:52 -0800

    resolve responsive display issue

--- js/app.js
+++ js/app.js
@@ -16,6 +16,31 @@ const img = $("coverImage");
 // Clear Canvas
 const clearCanvas = $("#clearCanvas");
 
+// Get the modal
+var modal = document.getElementById('rubricModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("rubricBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
